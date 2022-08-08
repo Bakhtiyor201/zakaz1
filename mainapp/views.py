ab40bb0b44a544bccf5b963774066729eb414305
Bakhtiyor201 <baxtiyorumarkulov2022acer@gmail.com>
2022-08-08 17:11:59 +0500

    birinchi commit

---
+++ mainapp/views.py
@@ -0,0 +1,23 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+from mainapp.models import Student
+from .forms import RegisterForm
+
+
+def mainview(request):    
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            name = form.data.get("name")
+            email = form.data.get("email")
+            course = form.data.get("course")
+            Student.objects.create(name=name, email=email, course=course)
+    form = RegisterForm()  
+    context = {
+        "form":form,
+                    }       
+    return render(request, 'index.html', context = context)
+
+def aboutview(request):
+    return render(request, 'about.html')    
